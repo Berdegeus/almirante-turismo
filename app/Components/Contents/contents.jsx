631bdbd709f334690c369f937904b03f4081e48b
Breno <breno.ortizdavid@outlook.com>
2023-06-14 11:37:59 -0300

    Contents Component Created and Hoespedagem route
    Contents Component created and added to Hospedagem route

---
+++ app/Components/Contents/contents.jsx
@@ -0,0 +1,64 @@
+import style from './contents.module.css'
+
+
+export default function Contents(){
+    return (
+        <div className={style.container}>
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+
+            </div>
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            
+            <div className={style.contentContainer}>
+                <img src="" alt="" />
+                <span className={style.contentName}></span>
+                <span className={style.avaliacao}></span>
+                
+            </div>
+            
+            
+        </div>
+    )
+}
---
+++ app/Hospedagem/page.js
@@ -0,0 +1,8 @@
+import Contents from './Components/Contents/contents'
+
+
+export default function Hospedagens() {
+    return (
+        <Contents></Contents>
+    )
+}
