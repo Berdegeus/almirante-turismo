15d3a2cf9de24b75eb343f328cdc7d4c0dcf325c
Bernardo <bernardo.geus@pucpr.edu.br>
2023-06-20 23:32:23 -0300

    Add footer

---
+++ app/Components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Logo from "../../../public/Logo.png";
+import "./footer.css";
+
+const Footer = () => (
+  <div className="react__footer section__padding">
+    <div className="react__footer-links">
+      <div className="react__footer-links_logo">
+        <Image src={Logo} alt="logo" height={169} width={300} />
+      </div>
+      <div className="react__footer-links_div">
+        <h4>Links</h4>
+        <p>Prefeitura</p>
+        <p>Midias sociais</p>
+        <p>Diario oficial</p>
+        <p>Contato</p>
+      </div>
+      <div className="react__footer-links_div">
+        <h4>Juridico</h4>
+        <p>Termos e Condições</p>
+        <p>Politica de Privacidade</p>
+        <p>Contato</p>
+      </div>
+      <div className="react__footer-links_div">
+        <h4>Entre em contato</h4>
+        <p>Bernardo Roorda de Geus</p>
+        <p>(41) 99119-8023</p>
+        <p>contato@benz.com</p>
+      </div>
+    </div>
+
+    <div className="react__footer-copyright">
+      <p>@2023 Benz. All rights reserved.</p>
+      <p>Almirante turimo</p>
+    </div>
+  </div>
+);
+
+export default Footer;
